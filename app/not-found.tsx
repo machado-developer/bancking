9999aae5929ee2f1b1c9a1453e8268b7ddb3862c
Antonio Machado <antonioulundomachado@outlook.com>
2024-12-11 14:06:33 +0000

    implementing initial files and folders structure

---
+++ app/not-found.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: "NOT FOUND- 404",
+    description: "Paginas inexistentes"
+}
+export default function NotFound() {
+    return (
+        <div>
+            Pagina n\ao encontraDA
+        </div>
+    )
+}
